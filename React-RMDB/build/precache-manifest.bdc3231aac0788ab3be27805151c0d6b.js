4ec9845bc1c20be8824d5a1a9701829de6a8e8cf
Jennifer <zengzhuoranjennifer@gmail.com>
2019-08-22 12:26:06 +0800

    add a new build

---
+++ React-RMDB/build/precache-manifest.bdc3231aac0788ab3be27805151c0d6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dcdee48b3b00ee8925416f85e36912",
+    "url": "/Movie-Search-Web-App/index.html"
+  },
+  {
+    "revision": "5a83ae67eb0d25c105a7",
+    "url": "/Movie-Search-Web-App/static/css/main.0b4a1a40.chunk.css"
+  },
+  {
+    "revision": "a866475c715671a25269",
+    "url": "/Movie-Search-Web-App/static/js/2.992b16ed.chunk.js"
+  },
+  {
+    "revision": "5a83ae67eb0d25c105a7",
+    "url": "/Movie-Search-Web-App/static/js/main.45b29a17.chunk.js"
+  },
+  {
+    "revision": "b5a14f17919a8c8084f7",
+    "url": "/Movie-Search-Web-App/static/js/runtime~main.e4b07c20.js"
+  }
+]);
